f5e619723a2d5b28cde089ed2836afa10a94350a
Krit2124 <kritserg2124@gmail.com>
2024-02-13 16:07:24 +0300

    react-router-blog

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import BlogPage from './components/blogPage';
+import Footer from './components/footer';
+import Header from './components/header';
+import MainPage from './components/mainPage';
+import WorkPageDetailed from './components/workPageDetailed';
+import WorksPage from './components/worksPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+
+      <Routes>
+        <Route path='/' Component={MainPage}/>
+        <Route path='/blogs' Component={BlogPage}/>
+        <Route path='/works' Component={WorksPage}/>
+        <Route path='/workDetailed' Component={WorkPageDetailed}/>
+      </Routes>
+
+      <Footer />
     </div>
   );
 }
